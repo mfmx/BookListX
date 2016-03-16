c5ea0622f7d9d2e179a0c6bfc67b09a4de495d71
Maria Xavier <maria.xavier@perficient.com>
2016-03-16 09:56:35 -0400

    fixing the create action issue

--- BookList/Models/PersonBook.cs
+++ BookList/Models/PersonBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace BookList.Models
 {
     public class PersonBook
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PersonBookID { get; set; }
 
         public string BookReadReview { get; set; }
